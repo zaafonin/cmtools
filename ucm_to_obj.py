7750139828bc9ac46f58c0fc47905a25b7119eb8
Zakhar Afonin <zakharafoniam@gmail.com>
2022-07-27 22:29:37 +0300

    Refactor .ucm to .obj

--- ucm_to_obj.py
+++ ucm_to_obj.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import argparse
+from multiprocessing.sharedctypes import Value
 import numpy as np
 
 
-poly = np.dtype([
+tri = np.dtype([
     ('v1', '<i4'),
     ('v2', '<i4'),
     ('v3', '<i4'),
@@ -12,108 +13,77 @@ vertex = np.dtype([
     ('x', np.float32),
     ('y', np.float32),
     ('z', np.float32),
-    ('i0', np.float32),
-    ('i1', np.float32),
-    ('wx', np.float32),
-    ('wy', np.float32),
-    ('wz', np.float32),
-])
-uv = np.dtype([
+    ('nx', np.float32),
+    ('ny', np.float32),
+    ('nz', np.float32),
     ('u', np.float32),
-    ('v', np.float32)
+    ('v', np.float32),
 ])
 
 parser = argparse.ArgumentParser(
-    description='Convert a .ucm to Wavefront .obj file.')
+    description='Convert a .ucm model to Wavefront .obj file.')
 parser.add_argument('src', type=str,
                     help='source file, .ucm')
 parser.add_argument('dest', type=str,
                     help='destination file, .obj')
-parser.add_argument('dest_mtl', type=str,
-                    help='generated material, .mtl')
-parser.add_argument('map', type=str, default='texture.png',
-                    help='texture map')
 args = parser.parse_args()
 
 with open(args.src, 'rb') as f_src:
-    print('Reading', args.src, '...')
-    b = f_src.read()
+    print(f'Reading {args.src} ...')
+    model_name = f_src.read(0x48)[0x08:].split(b'\x00')[0].decode()
+    num_frames, num_tris, num_verts, num_tags, idk = np.fromfile(f_src,
+                                                                 count=5,
+                                                                 dtype='<i4')
+    print(f'Model "{model_name}" loaded.')
+    print(
+        f'Frames:\t{num_frames}\n'
+        f'Tris:\t{num_tris}\n'
+        f'Verts:\t{num_verts}\n'
+        f'Tags:\t{num_tags}\n'
+    )
+    idx_buf = np.fromfile(f_src,
+                          count=num_tris,
+                          dtype=tri)
+    frames = []
+    ddframes = []
+    ddframes_ids = []
 
-    model_name = b[0x08:0x08+16]
-    num_polygons = np.frombuffer(b,
-                                 offset=0x4c,
-                                 count=1,
-                                 dtype='<i4')[0]
-    num_vertices = np.frombuffer(b,
-                                 offset=0x50,
-                                 count=1,
-                                 dtype='<i4')[0]
-    num_submodels = np.frombuffer(b,
-                                  offset=0x54,
-                                  count=1,
-                                  dtype='<i4')[0]
-    print(f'Model "{model_name.decode()}" loaded.')
-    print('Submodels:\t', num_submodels)
-    print('Polygons:\t', num_polygons)
-    print('Vertices:\t', num_vertices)
-
-    offset = 0x5c
-    print('I\toffset:', hex(offset))
-
-    i_buffer = np.frombuffer(b,
-                             offset=offset,
-                             count=num_polygons,
-                             dtype=poly)
-    offset += num_polygons * 4 * 3
-    print('V\toffset:', hex(offset))
-
-    v_buffer = np.frombuffer(b,
-                             offset=offset,
-                             count=num_vertices,
-                             dtype=vertex
-                             )
-    offset += num_vertices * 32
-    print('subm\toffset:', hex(offset))
+    for i in range(num_frames):
+        f_dd = []
+        f_dd_table = []
+        verts = np.fromfile(f_src,
+                            count=num_verts,
+                            dtype=vertex)
+        for i, v in enumerate(verts):
+            try:
+                ddframes_ids.append(ddframes.index((v['x'], v['y'], v['z'])))
+            except ValueError:
+                f_dd_table.append(i)
+                f_dd.append((v['x'], v['y'], v['z']))
+        frames.append(verts)
 
     try:
-        submodels = [b[offset + 16*i:offset + 16*i +
-                   16].split(b'\x00')[0].decode() for i in range(num_submodels)]
-        offset += num_submodels * 16
-        print('Submodels:', submodels)
+        tags = [f_src.read(0x16).split(b'\x00')[0].decode()
+                for i in range(num_tags)]
+        print(f'Tag names: \n{tags}\n')
     except Exception as e:
         # TODO: Exception handling
         print(e)
 
 with open(args.dest, 'w') as f_dest:
-    f_dest.write(f'mttlib {args.dest_mtl}\n')
+    for i, vtx_buf in enumerate(frames):
+        if len(frames) > 1:
+            f_dest.write(f"o {model_name}.{i + 1}\n")
+        else:
+            f_dest.write(f"o {model_name}\n")
 
-    for v in v_buffer:
-        #for field in vertex.names:
-        #    v[field] = round(v1[field], 2)
-        f_dest.write(f"v {v['x']} {v['y']} {v['z']}\n")
-
-    for v in v_buffer:
-        #for field in vertex.names:
-        #    v[field] = round(v1[field], 2)
-        f_dest.write(f"vt {v['wy']} {v['wz'] * -1}\n")
-
-    f_dest.write(f'usemtl mtl\n')
-
-    for i in i_buffer:
-        f_dest.write(
-            f"f {i['v1'] + 1}/{i['v1'] + 1} {i['v2'] + 1}/{i['v2'] + 1} {i['v3'] + 1}/{i['v3'] + 1}\n")
-
-print(f'Saved to "{args.dest}"!')
-
-# TODO: Proper materials
-with open(args.dest_mtl, 'w') as f_mtl:
-    f_mtl.write(f'''newmtl mtl
-    Ka 1.000000 1.000000 1.000000
-Kd 0.640000 0.640000 0.640000
-Ks 0.500000 0.500000 0.500000
-Ns 96.078431
-Ni 1.000000
-d 1.000000
-illum 0
-map_Kd {args.map}
-    ''')
+        for v in vtx_buf:
+            f_dest.write(f"v {v[0]} {v[1]} {v[2]}\n")
+        for v in vtx_buf:
+            f_dest.write(f"vt {v['u']} {v['v'] * -1}\n")
+        for v in vtx_buf:
+            f_dest.write(f"vn {v['nx']} {v['ny']} {v['nz']}\n")
+        for i in idx_buf:
+            f_dest.write(
+                f"f {i['v1'] + 1}/{i['v1'] + 1}/{i['v1'] + 1} {i['v2'] + 1}/{i['v2'] + 1}/{i['v2'] + 1} {i['v3'] + 1}/{i['v3'] + 1}/{i['v3'] + 1}\n")
+print(f'Complete!')
